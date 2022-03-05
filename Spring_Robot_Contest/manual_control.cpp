775d60ab44d3343d9297795b5b2c72c2d7c46525
Akihisa-Watanabe <57210762+Akihisa-Watanabe@users.noreply.github.com>
2022-03-05 21:58:11 +0900

    アラートの処理を追加

--- Spring_Robot_Contest/manual_control.cpp
+++ Spring_Robot_Contest/manual_control.cpp
@@ -21,13 +21,21 @@ PwmOut rack_push(PC_6);
 rack_pull.period(50);
 rack_push.period(50);
 
+PwmOut arm_rotate_1(PC_8); 
+PwmOut arm_rotate_2(PC_6);
+arm_rotate_1.period(50);
+arm_rotate_2.period(50);
+
+
+char can_data[4]={1,0,0,0};//CAN送信用の配列4byte
+
 /**
  * @fn
  * @brief アームを上下させる関数
  * @param (option) 0:アームを上げる(初期位置), 1:アームを下げる
  * @return int 処理が成功した場合は1を，失敗した場合は0を返す．
  */
-int move_arm(int option){
+int move_arm(char option){
     arm_down = 0;
     arm_up = 0;
     wait(5);
@@ -48,11 +56,21 @@ int move_arm(int option){
 
 /**
  * @fn
+ * @brief アームを回転させる関数
+ * @param 
+ * @return 
+ */
+int rotate_arm(char option){
+
+}
+
+/**
+ * @fn
  * @brief ラックを押し引きする関数
  * @param (option) 0:ラックを引く(初期位置), 1:ラックを押し出す
  * @return int 処理が成功した場合は1を，失敗した場合は0を返す．
  */
-int move_rack(int option){
+int move_rack(char option){
     rack_pull = 0;
     rack_push = 0;
     wait(5);
@@ -78,7 +96,12 @@ int move_rack(int option){
  * @return 
  */
 void alert(){
-    
+    can_data[0]=0;//エラーコード
+    if(can1.write(CANMessage(2,can_data,4))){
+        led = 1
+    }
+    led = 0;
+    can_data[1]=1;
 }
 
 
@@ -88,24 +111,28 @@ void alert(){
  */
 void control(){
     CANMessage msg;
-    int staus_arm, status_rack;
+    int status;
     
     if (can1.read(msg)){
-        if(msg.id == 0){
-            if (msg.data[0]==2){
-                status_arm = move_arm(msg.data[1]);
-                if (status_arm==0){
-                    //コントローラに異常を知らせる
-                }
-                status_rack = move_rack(msg.data[3]);
-                if (status_rack==0){
-                    //コントローラに異常を知らせる
-                }
+        if(msg.id == 2){
+            if (msg.data[0] != 0){
+                status = move_arm(msg.data[0]);              
+            }
+            else if (msg.data[1] != 0){
+                status = rotate_arm(msg.data[1])
+            }
+            else if (msg.data[2] != 0){
+                status = move_rack(msg.data[2]);
+            }
+
+            if (status == 0){
+                alert();
             }
         }
     }
 }
 
 int main(){
-    ticker.attach(&control,0.001)
+    wait(10);
+    ticker.attach(&control,0.001);
 }
