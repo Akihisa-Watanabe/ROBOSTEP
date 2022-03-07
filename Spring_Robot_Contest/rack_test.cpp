47f13ce1b4edef605a743eba064ec906c1d31473
Akihisa-Watanabe <57210762+Akihisa-Watanabe@users.noreply.github.com>
2022-03-07 17:36:22 +0900

    Update rack_test.cpp

--- Spring_Robot_Contest/rack_test.cpp
+++ Spring_Robot_Contest/rack_test.cpp
@@ -1,10 +1,6 @@
 #include "mbed.h"
 
 Ticker ticker;
-Timer ActiveTime; //タイマー計測用変数
-
-int move_rack(char option=0);
-void control();
 
 Serial pc(USBTX,USBRX);
 
@@ -23,8 +19,8 @@ int move_rack(char option){
     rack_pull = 0;
     rack_push = 0;
     wait(5);
+    int i=0;
     switch (option){
-            int i=0;
         case 0:
             while(i<=0.5){
                 i+=0.1;
@@ -45,13 +41,7 @@ int move_rack(char option){
     return 1;
 }
 void print_result(int status){
-    /*
-    if (status==0){
-        pc.printf("Success.\n");
-    }
-    else{
-        pc.printf("Error.\n");
-    }*/
+
 }
 
 /**
@@ -59,34 +49,13 @@ void print_result(int status){
  * @brief CAN通信で制御信号を受信してアームを制御する関数
  */
 void control(){
-        //int status;//,angle=0;
-    /*
-    printf("Arm up-down test.\n");
-    print_result(move_arm(0));  
-    print_result(move_arm(1));  
-
-    printf("Arm rotate test.\n");
-    angle=0;
-    print_result(rotate_arm(angle,angle+5));
-    angle=40;
-    print_result(rotate_arm(angle,angle+5));
-    */
-    //printf("Rack move test.\n");
     print_result(move_rack(0));
     print_result(move_rack(1));
-
-    //printf("Ultrasonic Sensor test.\n")
-    //auto_arm(100);
 }
 
 int main(){
-    //arm_down.period(0.05);
-    //arm_up.period(0.05);
-    //arm_rotate_1.period(0.05);
-    //arm_rotate_2.period(0.05);
     rack_pull.period(0.05);
     rack_push.period(0.05);
-    //move_arm(0);
     wait(10);
     ticker.attach(&control,0.001);
 }
